a85dd1a9e73df3410798eb066fcfeb6d9a8e255f
JeffreyYeet1 <jeffrey.ye@torontomu.ca>
2025-01-19 15:46:00 -0500

    Revert "ignore"
    This reverts commit 218e840c226d74c1b9389ec724d33bcea2d84e14.

--- src/pages/defaultwordle/components/row.tsx
+++ src/pages/defaultwordle/components/row.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import './row.css';
 
 interface RowProps {
@@ -7,29 +7,19 @@ interface RowProps {
 }
 
 const Row: React.FC<RowProps> = ({word, resultCode}) => {
-    const boxRef = useRef<HTMLDivElement>(null);
-    const [boxSize, setBoxSize] = useState<number>(100)
     const styles = [
         { backgroundColor: 'rgb(18,18,19)' },
         { backgroundColor: 'rgb(181,159,59)', outline: '2px solid rgb(181,159,59)'},
         { backgroundColor: 'rgb(83,141,78)', outline: '2px solid rgb(83,141,78)'}
     ]
 
-    const changeSize = () => {
-        if(boxRef.current) {
-            const currentSize = boxRef.current.getBoundingClientRect().height;
-
-            const newSize = currentSize * 1.1;
-            setBoxSize(newSize);
-        }
-    }
     const numberArray: number[] = resultCode.split('').map(Number);
     const boxes: number[] = [0,1,2,3,4];
 
     return(
         <>
             <div className="rowcontainer">
-                {boxes.map((num) => (<div style ={{...styles[numberArray[num]]}}>{word[num]}</div>))}
+                {boxes.map((num) => (<div style ={resultCode === '' && word[num] ? {outline: "2px solid rgb(86,87,88)"} : {...styles[numberArray[num]]}}>{word[num]}</div>))}
             </div>
         </>
     );
