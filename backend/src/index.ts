e4033b15012625d457ab19274fc8685d1df2ce23
JeffreyYeet1 <jeffrey.ye@torontomu.ca>
2025-02-28 20:25:25 -0500

    ignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -68,7 +68,7 @@ app.use('/api/auth', authRoutes);
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../frontend/build', 'index.html'));
 });
 
 // Start the server
